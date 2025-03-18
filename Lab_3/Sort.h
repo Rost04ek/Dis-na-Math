775062ccf4bab1140f8f396b02434196ee601c25
Rostyslav Kozachok <131653593+Rost04ek@users.noreply.github.com>
2025-03-18 13:06:45 +0200

    Delete Lab_3 directory

--- Lab_3/Sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-typedef std::vector<int> datatype;
-
-void SortBubble(datatype& array, int start, int end);
-void Swap(datatype& array, int i, int j);
-int MinItem(datatype& array, int start, int end, int min);
-void PrintArray(datatype& array); 
